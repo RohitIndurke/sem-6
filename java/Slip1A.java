be767db3bf2359790cde0b495f5374751ec48731
rohit <rohitindurke@gmail.com>
2026-04-03 18:53:48 +0530

    program

--- java/Slip1A.java
+++ java/Slip1A.java
@@ -1,5 +1,18 @@
-class Slip1A {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+class Slip1A extends Thread {
+
+    public void run() {
+        try {
+            for (char c = 'A'; c <= 'Z'; c++) {
+                System.out.println("here is Aplha " + c);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String args[]) {
+        Slip1A t1 = new Slip1A();
+        t1.start();
     }
 }
